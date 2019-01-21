8e20cf968a0fac4cd8d09b71becd69b95bdbd732
matie94 <matiemercier@gmail.com>
2019-01-21 10:09:52 +0100

    creation d'un nouveau code pour lier les 2 pages en 1

---
+++ formulairesecret.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8" />
+            <title>formulaire</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" type="text/css" media="screen" href="secret.css" />
+        </head>
+        <body>
+        	<p class="presentation"> <h1> Site de la NASA </h1><br>
+        		Site de la nullité acceptée sans apprentissage.<br><br><br><br>
+       			 Vous vous apprétez à rentrer sur un site ultra protégé... par un mot de passe!</p>
+
+        	<form method="post" action="secret.php">
+        		<input type="password" name="password">
+        		<input type="submit" value="Valider">
+        	</form>   
+        </body>
+    </html>
